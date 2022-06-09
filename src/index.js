9fa4c5e026cb366a34dbf62cb1b8f1901fd1ee30
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-06-09 16:20:19 -0500

    sass and bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { RememberizeApp } from './RememberizeApp';
 
+import './styles/styles.scss';
+
 ReactDOM.render(
   <React.StrictMode>
     <RememberizeApp />
