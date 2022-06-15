54d8e8ebcc1cf481db8d51eb11407755c844d437
Jose  luis Villa <vsjoseluisvs@gmail.com>
2022-06-14 23:39:49 -0500

    register screen

--- src/components/auth/RegisterScreen.jsx
+++ src/components/auth/RegisterScreen.jsx
@@ -1,7 +1,63 @@
-import React from 'react'
+import React from 'react';
 
 export const RegisterScreen = () => {
     return (
-        <div>RegisterScreen</div>
+        <div className='auth-form-container'>
+            <form className='auth-form'>
+                <div className="auth-form-content">
+                    <h3 className='auth-form-title mb-4'>Sign up</h3>
+                    
+                    <div className='form-group mt-3 mb-4'>
+                        <input
+                            type="text"
+                            className="form-control mt-1"
+                            placeholder='User name'
+                        />
+                    </div>
+
+                    <div className='form-group mt-3 mb-4'>
+                        <input
+                            type="email"
+                            className="form-control mt-1"
+                            placeholder='Email'
+                        />
+                    </div>
+
+                    <div className='form-group mt-3 mb-4'>
+                        <input
+                            type="number"
+                            className="form-control mt-1"
+                            placeholder='Age'
+                        />
+                    </div>
+
+                    <div className='form-group mt-3 mb-4'>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder='Password'
+                        />
+                    </div>
+
+                    <div className='form-group mt-3 mb-4'>
+                        <input
+                            type="password"
+                            className="form-control mt-1"
+                            placeholder='Confirm Password'
+                        />
+                    </div>
+
+                    <div className='d-grid gap-2 mt-3 mb-4'>
+                        <button type='submit' className='submit btn btn-primary'>
+                            Login
+                        </button>
+                    </div>
+
+                    <p className='forgot-password text-right mt-4'>
+                        Already have an account? <a href="/">Sign in here</a>
+                    </p>
+                </div>
+            </form>
+        </div>
     )
 }
