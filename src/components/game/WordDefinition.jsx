88b093203db9c91af7d1216a37f7c8cde35864a2
KeniaV123 <kenchi_kenia@hotmail.com>
2022-06-22 19:30:05 -0500

    Game Card

--- src/components/game/WordDefinition.jsx
+++ src/components/game/WordDefinition.jsx
@@ -1,23 +1,28 @@
 import React from 'react'
-import { Card, Button } from 'react-bootstrap'
-import img4 from '../../assets/img/book.jpg';
+import { Card } from 'react-bootstrap'
+import { gameImages } from '../../helpers/gameImages'
 
 export const WordDefinition = () => {
     return (
-        <div className="row justify-content-center">
-            <Card>
-                <Card.Header>Featured</Card.Header>
+        <div className="row justify-content-center" >
+            <Card className='game-card border-color'>
+                <Card.Header className="header-bg border-color">
+                <br></br>
+                </Card.Header>
                 <Card.Body>
-                    <Card.Title>Book</Card.Title>
+                    <Card.Title className="title-card" align="left"><strong>Book</strong></Card.Title>
                     <Card.Text>
                         Set of sheets joined to form a volume that is filled with different data to keep 
                         a record.
                     </Card.Text>
-                    
-                    <Card.Img className="image-definition" variant="bottom" src= {img4}  />
-                    <br></br>
-
-                    <Button variant="primary">Next</Button>
+                    <div className="col-12 text-center">
+                        <Card.Img className="image-definition"  variant="bottom" src={gameImages(`./book.jpg`)}  />
+                    </div>
+                    <div className='d-flex justify-content-end'>
+                        <button className='arrow-right'>
+                            <i className="fas fa-arrow-right"></i>
+                        </button>
+                    </div>
                 </Card.Body>
             </Card>
         </div>
