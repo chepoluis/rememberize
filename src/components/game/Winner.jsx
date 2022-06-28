48b0a2fbf9f90767e7fa39baafe8e22fba9d33c9
KeniaV123 <kenchi_kenia@hotmail.com>
2022-06-28 18:56:47 -0500

    fix winner screen

--- src/components/game/Winner.jsx
+++ src/components/game/Winner.jsx
@@ -7,16 +7,16 @@ export const Winner = () => {
     <div className="row justify-content-center" >
             <Card className='game-card border-color'>
                 <Card.Header className="header-bg border-color">
-                <br></br>
+                <br/>
                 </Card.Header>
                 <Card.Body>
                     <Card.Title className="title-winner" align="center"><strong>Good Job!</strong></Card.Title>
                     <div className="col-12 text-center">
                         <Card.Img className='trophy'  variant="bottom" src={gameImages(`./trophy.png`)}  />
                     </div>
-                    <br></br>
+                    <br/>
                     <div align="center">
-                    <h4><strong>Do you want to continue?</strong></h4> 
+                        <h4><strong>Do you want to continue?</strong></h4> 
                     </div>
                     <Button className='button-yes' variant="success">Yes</Button>
                     <Button className='button-no' variant="danger">No</Button>
