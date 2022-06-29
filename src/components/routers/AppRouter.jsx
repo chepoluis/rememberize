6487ed05265a24d382d95870bcb38da3b26f162f
KeniaV123 <kenchi_kenia@hotmail.com>
2022-06-28 20:55:31 -0500

    nice try screen

--- src/components/routers/AppRouter.jsx
+++ src/components/routers/AppRouter.jsx
@@ -3,11 +3,13 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { LoginScreen } from '../auth/LoginScreen';
 import { RegisterScreen } from '../auth/RegisterScreen';
 import { GameDefinitions } from '../game/GameDefinitions';
+import { NiceTry } from '../game/NiceTry';
 import { Winner } from '../game/Winner';
 import { WordDefinition } from '../game/WordDefinition';
 import { HomeScreen } from '../home/HomeScreen';
 import { InstructionsScreen } from '../instructions/InstructionsScreen';
 import { NavbarMain } from '../navbar/NavbarMain';
+
 
 
 export const AppRouter = () => {
@@ -24,6 +26,7 @@ export const AppRouter = () => {
                 <Route path='/game' element={ <WordDefinition /> } />
                 <Route path='/definitions' element={ <GameDefinitions /> } />
                 <Route path='/winner' element={ <Winner /> } />
+                <Route path='/nicetry' element={ <NiceTry /> } />
 
             </Routes>
         </BrowserRouter>
