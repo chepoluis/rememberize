f4bd170ef3990188a8d05026dbf3ed28ec20bf68
KeniaV123 <kenchi_kenia@hotmail.com>
2022-08-15 19:09:13 -0500

    delete titles

--- src/components/home/HomeScreen.jsx
+++ src/components/home/HomeScreen.jsx
@@ -40,7 +40,6 @@ export const HomeScreen = () => {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={`https://dummyimage.com/600x400/000/fff`} />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
                                     <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
@@ -54,7 +53,6 @@ export const HomeScreen = () => {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={`https://dummyimage.com/600x400/000/fff`} />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
                                     <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
@@ -68,7 +66,6 @@ export const HomeScreen = () => {
                             <Card style={{ width: '18rem' }}>
                                 <Card.Img variant="top" src={`https://dummyimage.com/600x400/000/fff`} />
                                 <Card.Body>
-                                    <Card.Title>Card Title</Card.Title>
                                     <Card.Text>
                                     Some quick example text to build on the card title and make up the
                                     bulk of the card's content.
